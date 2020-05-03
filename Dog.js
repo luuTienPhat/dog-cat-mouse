d5c3929823ce6fad2ec8e16282a7b0b39e81cec3
Phat <you@example.com>
2020-05-03 11:06:13 +0700

    bark method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,16 @@
-function Dog(){
+var chalk = require('chalk');
+
+function Dog(name){
+    this.name = name;
     this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat){
     this.stomach.push(cat);
 }
+
+Dog.prototype.bark = function(){
+    console.log('Gau Gau. My name is '+ chalk.yellow(this.name));
+}
+
+module.exports = Dog;
